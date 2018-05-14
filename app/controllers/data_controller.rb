0925b246867598e0beb6f6d24631edd7c699c06f
Jane Philipps <jane.philipps@gmail.com>
2018-05-13 21:29:18 -0700

    build first chart

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -1,18 +1,19 @@
-class DataController < ActionController::Base
-
-  # Data visualization for # of artists in collection based on birth year
-  # 3820 records with no birth year recorded
-  # 15549 - 3820 = 11,729 total records accounted for
-  # X Axis: Birth Year
-  # Y Axis: Total #
-
-  # Data format for chart:
-  # [{year: "1914", total: 4}, {year: "1916", total: 2}]
+class DataController < ApplicationController
 
   require "csv"
 
   def index
+  end
 
+  def chart
+    # Data visualization for # of artists in collection based on birth year
+    # 3820 records with no birth year recorded
+    # 15549 - 3820 = 11,729 total records accounted for
+    # X Axis: Birth Year
+    # Y Axis: Total #
+
+    # Data format for chart:
+    # [{year: "1914", total: 4}, {year: "1916", total: 2}]
     data = {}
 
     CSV.foreach("Artists.csv", :headers => true) do |row|
@@ -29,6 +30,13 @@ class DataController < ActionController::Base
     end
 
     @sorted_data = data.sort_by{|k, v| k}
+    @sorted_data.shift
+
+    respond_to do |format|
+      format.json {
+        render :json => @sorted_data
+      }
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/data", to: "data#index"
+  get "/data/index", to: "data#index"
+  get "/data/chart", :defaults => { :format => "json" }
 end
